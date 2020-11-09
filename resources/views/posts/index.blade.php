5cc260319e423bc7b85e682616270bc771bec49a
kokisgithub <kmagastbsb.nvng816@gmail.com>
2020-11-10 03:58:03 +0900

    アプリケーション作成の準備
    .envファイルの編集、データベース作成、migrationファイルの作成及びカラムの追加、tinkerでデータ追加、Mass Assignmentの設定、
    ルーティングの設定、コントローラ作成、.htaccessの編集とstorage/以下のパーミッションの変更

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>My Top10</title>
+</head>
+<body>
+  <div class="container">
+    <h1>My Top10</h1>
+    <ul>
+      <li><a href="">title</a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </div>
+</body>
+</html>
