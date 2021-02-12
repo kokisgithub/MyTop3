9f8910fb40cc438ebde611b2f5783b8906254440
kokisgithub <kmagastbsb.nvng816@gmail.com>
2021-02-12 19:18:25 +0900

    記事一覧画面のナビバーに管理者用ログインページのリンクを追加。
    同上

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -21,10 +21,13 @@
             <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
           </li>
           @if (Route::has('register'))
-            <li class="nav-item">
+            <li class="nav-item mr-5">
               <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
             </li>
           @endif
+          <li class="nav-item">
+              <a class="nav-link text-warning" href="{{ route('admin.login') }}">{{ __('Admin') }}</a>
+          </li>
         @else
           <li class="nav-item dropdown">
             <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
