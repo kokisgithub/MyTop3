43e3204782a905a96b6266dcbe5cec867cd0d641
kokisgithub <70275253+kokisgithub@users.noreply.github.com>
2021-03-13 19:30:46 +0900

    画像アップロード画面へのリンクの作成

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -60,8 +60,7 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <!-- Left Side Of Navbar -->
-    <ul class="navbar-nav mr-auto">
-    
+    <ul class="navbar-nav mr-auto">    
     </ul>
 
     <!-- Right Side Of Navbar -->
