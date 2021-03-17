6bf4257498e0c0afbea88ae871804f26968a31eb
kokisgithub <kmagastbsb.nvng816@gmail.com>
2021-03-17 23:43:36 +0900

    HTTPテストの準備 #6
    artisanコマンドによる新しいテストファイル、テスト用DB、.envファイルをコピーした.env.testingを作成し、
    artisanコマンド(migrate --env=testing)によるテスト用DBへのマイグレーションを実行したため。

---
+++ tests/Feature/SampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->withoutExceptionHandling();
+        $this->assertTrue(true);
+        
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
